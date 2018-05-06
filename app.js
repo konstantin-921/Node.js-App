16531feeccc6657986923c6a7e47a99280731082
Kostya Podlesniy <konstantin-921@yandex.ru>
2018-05-06 14:09:59 +0300

    JWT not working!

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
+var _ = require("lodash");
 var express = require('express');
 var bodyParser = require('body-parser')
 var app = express();
 var Sequelize = require('sequelize');
-var session = require('express-session');
+var jwt = require('jsonwebtoken');
 var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy
+var passportJWT = require("passport-jwt");
+
+var ExtractJwt = passportJWT.ExtractJwt;
+var JwtStrategy = passportJWT.Strategy;
+
 
 const sequelize = new Sequelize('app', 'nodejs', '1111', {
   dialect: 'postgres',
@@ -12,27 +17,35 @@ const sequelize = new Sequelize('app', 'nodejs', '1111', {
   port: 5432
 });
 
-var Users = sequelize.define('users', {
-  id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
-  name: Sequelize.TEXT,
-  password: Sequelize.INTEGER
-})
+var jwtOptions = {}
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+jwtOptions.secretOrKey = 'tasmanianDevil';
+
+var strategy = new JwtStrategy(jwtOptions, function(jwt_payload, next) {
+  console.log('payload received', jwt_payload);
+  sequelize.query("SELECT id, name, password FROM users", {type: sequelize.QueryTypes.SELECT})
+  .then((users) => {
+    var user = users[_.findIndex(users, {id: jwt_payload.id})];
+    if (user) {
+      next(null, user);
+    } else {
+      next(null, false);
+    }
+   });
+  });
+
+passport.use(strategy);
+
+
+app.use(passport.initialize());
 
 app.use(express.static('public'));
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // parse application/json
 app.use(bodyParser.json())
-app.use(session({
-  secret: 'cat',
-  resave: false,
-  saveUninitialized: false
-}))
-app.use(passport.initialize())
-app.use(passport.session())
-
 
 app.get('/home', function (req, res) {
   res.send(JSON.stringify("Login successful!"));
@@ -46,24 +59,25 @@ app.post('/login', function (req, res) {
    query(req, res);
 });
 
+app.get("/secret", passport.authenticate('jwt', { session: false }), function(req, res){
+  res.json("Success! You can not see this without a token");
+});
+
 app.post('/registred', function (req, res) {
   querySignUp(req, res);
 });
 
 function query(req, res) {
-  sequelize.query("SELECT name, password FROM users", {type: sequelize.QueryTypes.SELECT})
-  .then(users => {
-    var array = [];
-    for(var i = 0; i < users.length; i++) {
-      array.push(users[i]);
-    }
-    return array;
-  })
-  .then((array) => {
-    for(var i = 0; i < array.length; i++){
-      if(array[i].name === req.body.username && array[i].password === Number(req.body.userpass)) {
-        res.send(JSON.stringify(""));
-      }
+  sequelize.query("SELECT id, name, password FROM users", {type: sequelize.QueryTypes.SELECT})
+  .then((users) => {
+    var user = users[_.findIndex(users, {name: req.body.username, password: Number(req.body.userpass)})];
+    
+    if(user.password === Number(req.body.userpass)) {
+    // from now on we'll identify the user by the id and the id is the only personalized value that goes into our token
+      var payload = {id: user.id};
+      var token = jwt.sign(payload, jwtOptions.secretOrKey);
+      res.json({message: "ok", token: token});
+    console.log(token);
     }
   })
   .catch((error) => {
@@ -74,7 +88,7 @@ function query(req, res) {
 function querySignUp(req, res) {
   sequelize.query("CREATE TABLE IF NOT EXISTS users (id serial PRIMARY KEY, name text, password integer);")
   .then(() => {
-    sequelize.query("INSERT INTO users (name, password) VALUES ('"+ req.body.username +"', '"+ req.body.userpass +"')")
+    sequelize.query(`INSERT INTO users (name, password) VALUES ('${req.body.username}', ${req.body.userpass})`)
   })
 }
 
