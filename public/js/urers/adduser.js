38475bdd495b9e558bbb1aa3b878d391f4b98d52
Kostya Podlesniy <konstantin-921@yandex.ru>
2018-06-22 17:19:28 +0300

    Small fix

--- public/js/urers/adduser.js
+++ public/js/urers/adduser.js
@@ -1,4 +1,4 @@
-const Registred = (function() {
+const Registred = (function () {
 
   function Registred() {
 
@@ -8,18 +8,18 @@ const Registred = (function() {
     const btnRegistred = document.getElementById('btnReg');
     const help = new Help();
 
-    if(btnRegistred) {
-      btnRegistred.addEventListener('click', function(event) {
+    if (btnRegistred) {
+      btnRegistred.addEventListener('click', function (event) {
         event.preventDefault();
-  
-      if(inputNameSign.value != '' && inputPassSign.value != '' && inputEmailSign.value != '') {
+
+        if (inputNameSign.value != '' && inputPassSign.value != '' && inputEmailSign.value != '') {
           let userSignUp = {
             username: inputNameSign.value,
             userpass: inputPassSign.value,
             useremail: inputEmailSign.value
           }
-    
-          fetch('/users', { 
+
+          fetch('/users', {
             method: 'POST',
             headers: {
               "Content-Type": "application/json"
@@ -27,16 +27,16 @@ const Registred = (function() {
             body: JSON.stringify(userSignUp),
             redirect: 'follow'
           })
-          .then(help.checkStatus)
-          .then(help.parseJSON)
-          .then(function(response) {
-            const form = document.getElementById('signUpForm').remove();
-            const text = response.error||response;
-            const message = response.error ? new UserMessage(text, false) : new UserMessage(text, true);
-          })
-          .catch(function(error) {
-            console.log(error);
-          })
+            .then(help.checkStatus)
+            .then(help.parseJSON)
+            .then(function (response) {
+              const form = document.getElementById('signUpForm').remove();
+              const text = response.error || response;
+              const message = response.error ? new UserMessage(text, false) : new UserMessage(text, true);
+            })
+            .catch(function (error) {
+              console.log(error);
+            })
         } else {
           const text = 'Все поля должны быть заполнены';
           const message = new UserMessage(text, false);
@@ -47,4 +47,4 @@ const Registred = (function() {
 
   return Registred;
 
-}) ()
+})()
--- route/users.js
+++ route/users.js
@@ -8,6 +8,7 @@ router.post('/users', function (req, res, next) {
 });
 
 function hash(req, res, next) {
+  console.log(req.body);
   let passwordFromUser = req.body.userpass;
   let salt = bcrypt.genSaltSync(10);
   let passwordToSave = bcrypt.hashSync(passwordFromUser, salt);
@@ -16,22 +17,22 @@ function hash(req, res, next) {
 }
 
 async function addUser(req, res, next) {
-  try {  
+  try {
     let search = await sequelize.query(`SELECT *  FROM users WHERE name = '${req.body.username}' `, { type: sequelize.QueryTypes.SELECT });
-    if(search[0] === undefined) {
-      let data = await sequelize.query(`INSERT INTO users (password, name, email) VALUES (:userpass, :username, :useremail)`, {replacements: {userpass: req.body.userpass, username: req.body.username, useremail: req.body.useremail}});
+    if (search[0] === undefined) {
+      let data = await sequelize.query(`INSERT INTO users (password, name, email) VALUES (:userpass, :username, :useremail)`, { replacements: { userpass: req.body.userpass, username: req.body.username, useremail: req.body.useremail } });
       res.json("Successful registration!");
     } else {
-      res.json({error: 'Такой пользователь уже существует'});
+      res.json({ error: 'Такой пользователь уже существует' });
     }
-  } catch(error){
+  } catch (error) {
     next(error);
   }
 }
 
 
 
-router.get('/followers', function(req, res, next) {
+router.get('/followers', function (req, res, next) {
   findUser(req, res, next);
 })
 
@@ -39,54 +40,54 @@ async function findUser(req, res, next) {
   try {
     let id = req.query.id;
     let letter = req.query.letter;
-    let data = await sequelize.query(`SELECT name, id FROM users WHERE name ILIKE :search and NOT id = :id`, {replacements: {search : `${letter}%`, id: id}, type: sequelize.QueryTypes.SELECT});
+    let data = await sequelize.query(`SELECT name, id FROM users WHERE name ILIKE :search and NOT id = :id`, { replacements: { search: `${letter}%`, id: id }, type: sequelize.QueryTypes.SELECT });
     res.json(data);
-  } catch(error) {
+  } catch (error) {
     next(error);
   }
 }
 
 
 
-router.post('/followers', function(req, res,next) {
+router.post('/followers', function (req, res, next) {
   followUser(req, res, next);
 })
 
 async function followUser(req, res, next) {
   try {
-    await sequelize.query(`INSERT INTO followers (follower, following) VALUES (:follower, :following)`, {replacements: {follower: req.body.userId, following: req.body.id}});
+    await sequelize.query(`INSERT INTO followers (follower, following) VALUES (:follower, :following)`, { replacements: { follower: req.body.userId, following: req.body.id } });
     res.json("Success!");
-  } catch(error) {
+  } catch (error) {
     next(error);
   }
 }
 
 
 
-router.delete('/followers', function(req, res, next) {
+router.delete('/followers', function (req, res, next) {
   deletefollow(req, res, next);
 })
 
 async function deletefollow(req, res, next) {
   try {
-    await sequelize.query(`DELETE FROM followers WHERE following = :following and follower = :follower`, {replacements: {following: req.body.id, follower: req.body.userId}});
+    await sequelize.query(`DELETE FROM followers WHERE following = :following and follower = :follower`, { replacements: { following: req.body.id, follower: req.body.userId } });
     res.json("Success!");
-  } catch(error) {
+  } catch (error) {
     next(error);
   }
 }
 
 
 
-router.get('/followers/teststate', function(req, res, next) {
+router.get('/followers/teststate', function (req, res, next) {
   teststate(req, res, next);
 })
 
 async function teststate(req, res, next) {
   try {
-    let data = await sequelize.query(`SELECT follower, following FROM followers`, {type: sequelize.QueryTypes.SELECT});
+    let data = await sequelize.query(`SELECT follower, following FROM followers`, { type: sequelize.QueryTypes.SELECT });
     res.json(data);
-  } catch(error){
+  } catch (error) {
     next(error);
   }
 }
